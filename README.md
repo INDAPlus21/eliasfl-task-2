636cac29b33e4d4aa0e993fe71871db9acc7db6b
Elias <elias@floreteng.se>
2021-09-12 18:37:41 +0200

    sum_numbers optimization & comments

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # eliasfl-task-2
 
-# Summera tal
+## Summera tal
 
 Crate name (subfolder): [`sum_numbers`](sum_numbers/)
-
 Assignment: https://kth.kattis.com/problems/kth.javap.sumsort
-
 Submission proof:
-
 ![test](sum_numbers/proof.png)
+
+## Avstånd till kanten
+
+Crate name (subfolder): [`dist_to_side`](dist_to_side/)
+Assignment: https://kth.kattis.com/problems/kth.javap.kant
+Submission proof:
+![test](dist_to_side/proof.png)
--- sum_numbers/src/main.rs
+++ sum_numbers/src/main.rs
@@ -1,23 +1,31 @@
 use std::io::{self, BufRead};
 
 fn main() {
-    let stdin = io::stdin();
-    let mut lines = stdin.lock().lines().map(|_line| _line.ok().unwrap());
+    let stdin = io::stdin(); // Get stdin
 
-    let to_read = lines.next().unwrap().parse::<usize>().unwrap();
+    // Define iterator with input lines from stdin
+    let mut lines = stdin.lock().lines().map(|line| line.ok().unwrap());
 
+    // Read first line and parse as `usize` integer
+    let to_read: usize = lines.next().unwrap().parse().unwrap();
+
+    // Get next line, split by whitespace, parse as `i32` and collect to vector
     let mut numbers: Vec<i32> = lines
         .next()
         .unwrap()
         .split_whitespace()
         .map(|num| num.parse::<i32>().unwrap())
         .collect();
+    // Slice numbers to only get first `to_read` numbers, even if more are entered
     numbers = numbers[0..to_read].to_vec();
     numbers.sort();
 
-    let middle_index = (to_read as f64 / 2 as f64).ceil() as usize;
-    let bigger_half = &numbers[(to_read - middle_index)..];
+    // Find how many number to sum (middle of vector, including centermost element if odd)
+    let to_sum = (to_read + 1) / 2;
+    // Slice right half, from middle number to end
+    let bigger_half = &numbers[(to_read - to_sum)..];
 
+    // Sum `bigger_half` vector
     let sum: i32 = bigger_half.iter().sum();
     println!("{}", sum)
 }
